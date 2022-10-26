1c8d0f2b041455b0040cf5b9004d22b5fd22cfc6
the.phamdinh <the.phamdinh@vti.com.vn>
2022-10-26 17:14:47 +0700

    add

--- src/main/java/com/example/registration_service/RegistrationServiceApplication.java
+++ src/main/java/com/example/registration_service/RegistrationServiceApplication.java
@@ -2,13 +2,16 @@ package com.example.registration_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
+//@SpringBootApplication
+//@EnableEurekaClient
+//@EnableFeignClients
+@EnableEurekaServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RegistrationServiceApplication {
 
 	public static void main(String[] args) {
